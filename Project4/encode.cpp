b5368e78d7b2a7dd29d8398041b116c1faaaa85b
XJ Peng <pengx4@rpi.edu>
2023-11-06 17:00:13 -0500

    file update

---
+++ Project4/encode.cpp
@@ -0,0 +1,5 @@
+#include "encode.h"
+
+void encode(string word, long index){
+    
+}
---
+++ Project4/encode.h
@@ -0,0 +1,9 @@
+#ifndef ENCODE_H_
+#define ENCODE_H_
+#include <string>
+using namespace std;
+
+
+void encode(string word, long index);
+void decode(string encodedWord);
+#endif
